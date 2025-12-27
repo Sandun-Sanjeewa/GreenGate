630b77f13e7ea6fa1edb3000d2a6577f7fd6148f
Sandun Sanjeewa <sanjeewasandun82@gmail.com>
2025-12-27 17:37:09 +0530

    signup-form is started

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,9 @@
-
-
+import SignupForm from "./components/SignupForm"
 function App() {
-  
-
   return (
     <>
       <div className="bg-amber-300 h-screen ">
-
+        <SignupForm />
       </div>
     </>
   )
---
+++ Client/src/components/SignupForm.jsx
@@ -0,0 +1,11 @@
+const SignupForm = () =>{
+    return(
+        <>
+        <div className="h-full w-full bg-blue-800">
+
+        </div>
+        </>
+    );
+};
+
+export default SignupForm;
