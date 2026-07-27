3f1c68d11c9cd0007b80471cb4cf3d2d412419f1
Sandun Sanjeewa <sanjeewasandun82@gmail.com>
2026-07-27 17:08:36 +0530

    created user signup form

--- Client/src/components/SignupForm.jsx
+++ Client/src/components/SignupForm.jsx
@@ -1,11 +1,56 @@
+import { useState } from "react";
+
 const SignupForm = () => {
+    const [userSignUpData, setUserSignUpData] = useState({
+        name:"",
+        email:"",
+        password:"",
+        role:"user"
+    });
+    const [error, setError] = useState({
+        name:"",
+        email:"",
+        password:"",
+        general:""
+    });
+
+    const handleChange = (e) =>{
+        const {name,value} = e.target;
+        setUserSignUpData(prev => ({...prev, [name]:value}));
+        setError(prev => ({...prev, [name]:"",general:""}));
+    };
+
+    const validate = () => {
+        const newErrors = {};
+        if (!userSignUpData.name.trim()) newErrors.name = "Name is required";
+        if (!userSignUpData.email.trim()) newErrors.email = "Email is required";
+        else if (!/^\S+@\S+\.\S+$/.test(userSignUpData.email)) newErrors.email = "Email forat is invalid";
+        if (!userSignUpData.password.trim()) newErrors.password = "Password is required";
+        else if (!userSignUpData.password.length < 6) newErrors.password = "Password must be at least 6 characters";
+        return newErrors;
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const validationErrors = validate();
+        if (Object.keys(validationErrors).length > 0){
+            setError(prev => ({...prev, ...validationErrors}));
+            return;
+        }
+
+        try {
+            
+        } catch (err) {
+            
+        }
+    };
+
     return (
         <>
-            <div className="h-full w-full bg-blue-800">
-                <div className="h-36 w-full bg-black">
-                    <h1 className="text-2xl text-white">Frontend Docker</h1>
+            <div className="">
+                <div className="md:mb-4">
+                    <h3 className="text-gray-500 md:text-sm">Create new Account</h3>
                 </div>
-
             </div>
         </>
     );
